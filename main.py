569d5c5426b031389e45b92eef4a345c800a9503
DonTihi <tihi.akos@gmail.com>
2024-03-26 14:10:22 +0100

    Add gitignore

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def openAI():
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."}
-           # {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
         ]
     )
     return (print(completion.choices[0].message))
